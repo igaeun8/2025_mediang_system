bb14e6b611c14db00ab330a21289d4c1f330f98c
igaeun8 <145009182+igaeun8@users.noreply.github.com>
2025-07-08 12:18:05 +0900

    Create vectorStore.js

---
+++ back/server/services/vectorStore.js
@@ -0,0 +1,24 @@
+// chroma 벡터 저장 및 검색 모듈ㄹ
+
+import { Chroma } from "langchain_chroma";
+import embeddings from "./embeddingService.js";
+
+const persistDirectory = "./chroma";
+const collectionName = "chroma-menual";
+
+export async function initVectorStore(documents) {
+  return await Chroma.fromDocuments({
+    documents,
+    embedding: embeddings,
+    collectionName,
+    persistDirectory,
+  });
+}
+
+export async function loadVectorStore() {
+  return new Chroma({
+    collectionName,
+    persistDirectory,
+    embeddingFunction: embeddings,
+  });
+}
